8b2c4b8eadf17a4f29e4a6a4bff1db3adfac7d7d
mobresu <mobresu@gmail.com>
2019-11-05 20:38:24 -0400

    banner
    banner

--- hit/script.js
+++ hit/script.js
@@ -5,3 +5,43 @@ var animation = bodymovin.loadAnimation({
     autoplay: true,
     path: 'data.json'
 })
+
+jQuery(document).ready(function( $ ){
+  
+  niks();
+  
+  jQuery('.g1-drop-toggle-icon').click(function(){
+    skin();
+  })
+  
+  
+  function skin(){
+  	if ( $(".g1-skinmode").length > 0 ) {
+      jQuery('.g1-hb-row-b>.g1-row-background').css({"background-image":"url('http://www.lasopa.com/wp-content/uploads/2019/05/fse2.jpg')"})
+    }else{
+      jQuery('.g1-hb-row-b>.g1-row-background').css({"background-image":"none"})
+    };
+  }
+  
+  function niks(){
+  	if ( $(".g1-skinmode").length <= 0 ) {
+      jQuery('.g1-hb-row-b>.g1-row-background').css({"background-image":"url('http://www.lasopa.com/wp-content/uploads/2019/05/fse2.jpg')"})
+    }
+  }
+  
+  
+    
+    
+    jQuery( window ).scroll(function() {
+  	if ( jQuery(".is-sticky").length > 0 ) {
+        alert("up");
+      /*jQuery('.site-branding img').css({"width":"240"})*/
+    }else{
+        alert("down");
+      /*jQuery('.site-branding img').css({"width":"130"})*/
+    };
+});
+    
+    
+  
+});
